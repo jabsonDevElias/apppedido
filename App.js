0570f42a518dd4bb0c74e88aecbb074c38cf58d1
jabsonDevElias <jabsonelias@gmail.com>
2024-03-26 21:24:16 -0300

    Feat(App): Primeiro commit

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
+
+CarregarProdutos = async () => {
+  try {
+      const response = await axios.post('http://10.0.2.2:3003/produtos');
+      return response.data;
+  } catch(err) {
+      // TODO
+      console.error(err); 
+  }
+};
 
 export default function App() {
+
+  const [produtos,setProdutos] = React.useState(CarregarProdutos());
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {produtos._j.map(item => <View style={{backgroundColor:"red",marginTop:10,padding:10}}><Text key={item.id}>{item.prod_tx_nome}</Text></View>)}
       <StatusBar style="auto" />
     </View>
   );
